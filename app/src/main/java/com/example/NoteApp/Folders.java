86f222ff76e1cb72448a6efdabe72f86de8d126f
PhilScott4VT <PhilScott4USA@gmail.com>
2021-05-06 16:26:25 -0400

    Merging ActionBar commit

--- app/src/main/java/com/example/NoteApp/Folders.java
+++ app/src/main/java/com/example/NoteApp/Folders.java
@@ -1,10 +1,13 @@
 package com.example.NoteApp;
 
+import android.app.ActionBar;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -30,6 +33,8 @@ public class Folders extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_folders);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         //TODO replace with sql
         ListView listView = findViewById(R.id.listView);
@@ -86,5 +91,20 @@ public class Folders extends AppCompatActivity {
                 return true;
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
     }
 }
--- app/src/main/java/com/example/NoteApp/NoteEditor.java
+++ app/src/main/java/com/example/NoteApp/NoteEditor.java
@@ -2,12 +2,15 @@ package com.example.NoteApp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 
 import java.util.HashSet;
@@ -22,6 +25,8 @@ public class NoteEditor extends AppCompatActivity {
         setContentView(R.layout.activity_note_editor);
 
         EditText editText = findViewById(R.id.editText);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         //Fetch data that is passed from Folders
         Intent intent = getIntent();
@@ -58,5 +63,20 @@ public class NoteEditor extends AppCompatActivity {
                 // add your code here
             }
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
     }
 }
