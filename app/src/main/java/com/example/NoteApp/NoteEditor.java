f707d0a980581b3d158c129044e7b8699f50ab4b
PhilScott4VT <PhilScott4USA@gmail.com>
2021-05-12 19:00:06 -0400

    test

--- app/src/main/java/com/example/NoteApp/NoteEditor.java
+++ app/src/main/java/com/example/NoteApp/NoteEditor.java
@@ -24,45 +24,7 @@ public class NoteEditor extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_editor);
 
-        EditText editText = findViewById(R.id.editText);
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        //Fetch data that is passed from Folders
-        Intent intent = getIntent();
-
-        //Opens data using key and value
-        noteId = intent.getIntExtra("noteId", -1);
-        if (noteId != -1) {
-            editText.setText(Folders.notes.get(noteId));
-        } else {
-            Folders.notes.add("");
-            noteId = Folders.notes.size() - 1;
-            Folders.arrayAdapter.notifyDataSetChanged();
-        }
-
-        editText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                Folders.notes.set(noteId, String.valueOf(charSequence));
-                Folders.arrayAdapter.notifyDataSetChanged();
-
-                // Creating Object of SharedPreferences to store data in the phone
-                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.notes", Context.MODE_PRIVATE);
-                HashSet<String> set = new HashSet(Folders.notes);
-                sharedPreferences.edit().putStringSet("notes", set).apply();
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-                // add your code here
-            }
-        });
     }
 
     @Override
